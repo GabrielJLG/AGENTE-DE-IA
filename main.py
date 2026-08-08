69b099b8457a0e98c796d8f00b6d17870129dfe6
GabrielJLG <gabrieltech008@gmail.com>
2026-08-08 15:55:07 -0300

    Update main.py

--- main.py
+++ main.py
@@ -29,7 +29,7 @@ if st.button('enviar'):
         messages=[
         {
         'role':'system',
-        'content':"Você é um agente especializado em revisão de código. Analise o código enviado, enontre bugs, erros, problemas de segurança código desnecessário e de sugestão de melhorias."
+        'content':"Você é um agente especializado em revisão de código. Analise o código enviado, encontre bugs, erros, problemas de segurança, código desnecessário e de sugestão de melhorias."
         },
         {
             'role':'user',
