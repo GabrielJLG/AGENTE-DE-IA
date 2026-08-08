69d505ebc8484cc27fa8522283a6d921988fa4e3
GabrielJLG <gabrieltech008@gmail.com>
2026-08-08 14:40:09 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,37 +1,48 @@
 from groq import Groq
-from modulo.submodulo.funcoes import mostar_text
-
-
-text  =  input('Digite um texto: ')
-
-
-print(mostar_text(text))
-
+import streamlit as st 
+import time
+import os
 
 
 
 # pip install groq 
-while True: 
-    client = Groq (
-    api_key = "gsk_eNMJDWgsk_Qt6KUvQETw5CWAGr7E3aWGdyb3FYzzbmN9OR1SzkHsxQZDNkcwmA"
-    )
 
 
-    pergunta =  input('pergunta: ')
-    reposta =  client.chat.completions.create(
-    model = "llama-3.3-70b-versatile",
-    temperature=0.7,
-    messages=[
-    {
-    'role':'system',
-    'content':"Você é um piloto de avião."
-    },
-    {
-        'role':'user',
-        'content': pergunta
+
+
+client = Groq(
+    api_key=os.environ.get("GROQ_API_KEY"),
+)
+
+
+st.title("Conversa com o Piloto") 
+pergunta  = st.text_input('pergunta:')
+
+
+if st.button('enviar'):
+    # if pergunta.strip():
+        reposta =  client.chat.completions.create(
+        model = "llama-3.3-70b-versatile",
+        # temperature=0.7,
+
+
+        messages=[
+        {
+        'role':'system',
+        'content':"Você é um piloto de avião experiente."
+        },
+        {
+            'role':'user',
+            'content': pergunta
+            
+        }
+        ]
+        )
+
+
+        st.text(reposta.choices[0].message.content)
+        time.sleep(0)
         
-    }
-    ]
-    )
-    print(reposta.choices[0].message.content)
+
+
 
